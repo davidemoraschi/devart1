01eaf88986b6ee41df91381b4c599f9a4f8fe90c
davidemoraschi <davidem@eurostrategy.net>
2016-08-23 14:54:47 +0200

    moved to GitHub

---
+++ devart1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Devart.Data.Oracle;
+
+namespace devart1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //OracleConnection oracleConnection1 = new OracleConnection();
+            //oracleConnection1.Server = "DESARROLLO";
+            //oracleConnection1.UserId = "scott";
+            //oracleConnection1.Password = "tiger";
+
+            OracleConnectionStringBuilder oraCSB = new OracleConnectionStringBuilder();
+            oraCSB.Direct = true;
+            oraCSB.Server = "192.168.0.1";
+            oraCSB.Port = 1251;
+            oraCSB.Sid = "OracleSid";
+            oraCSB.UserId = "scott";
+            oraCSB.Password = "tiger";
+            oraCSB.MaxPoolSize = 150;
+            oraCSB.ConnectionTimeout = 30;
+            OracleConnection myConnection = new OracleConnection(oraCSB.ConnectionString);
+
+
+        }
+    }
+}
